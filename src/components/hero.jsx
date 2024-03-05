0e673cdddaf46417400f0ffd9fb549a05c1093ba
arfianarif <arfianarif11@gmail.com>
2024-03-05 22:51:11 +0700

    feat: add on click button to about

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,7 @@
+import useScroll from "../hooks/useScroll"
 
 const Hero = () => {
+  const {setScroll} = useScroll()
   return (
     <section id="hero" className="relative z-10 flex flex-col items-center justify-center min-h-screen py-20"
     data-aos="zoom-in-down" data-aos-duration="1000">
@@ -10,8 +12,8 @@ const Hero = () => {
       <p className="mb-8 text-sm leading-relaxed md:text-base lg:text-lg "><b>MultiLIVE,</b> platform is a leading platform that enables you to broadcast live with more than one video source simultaneously. Enjoy the convenience of streaming pre-recorded content with <b>MultiLIVE!</b>
       </p>
       <div className="flex justify-center">
-        <a href="#about"
-          className="transition duration-200 inline-block py-2 px-6 rounded-lg shadow ease-out font-bold bg-yellow-400 !text-noir hover:bg-yellow-400 ">Learn more</a>
+        <button type={'button'} onClick={() => setScroll('about')}
+          className="transition duration-200 inline-block py-2 px-6 rounded-lg shadow ease-out font-bold bg-yellow-400 !text-noir hover:bg-yellow-400 ">Learn more</button>
       </div>
     </div>
   </section>
